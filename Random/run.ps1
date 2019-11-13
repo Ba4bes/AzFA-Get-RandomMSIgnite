e8d2d131e6914ffdc049c07f26d5c34ca88db30f
Barbara <ba@4bes.nl>
2019-11-13 16:22:04 +0100

    Finished development

--- Random/run.ps1
+++ Random/run.ps1
@@ -60,11 +60,21 @@ foreach ($property in $session.psobject.Properties) {
 </table>
 <Br><A Href = $URL>Click here to view the session</a> or Reload for a new suggestion
 <Br><Br><font size=2>This site is not created or affiliated by Microsoft. This is just a tribute.
-<Br> This site is running on an Azure Function App with PowerShell. Want to create your own? Click <a href = 'https://4bes.nl/MSIgnite'>here</a>
-<Br><Br>
+<Br> This site is running on an Azure Function App with PowerShell. Want to find out more about creating your own? Click <a href = 'https://4bes.nl/MSIgnite'>here</a><Br>
+<Br>
 Barbara Forbes<br>
 @Ba4bes<br>
-<a href=https://4bes.nl>4bes.nl</a></font size=2>
+<a href=https://4bes.nl>4bes.nl</a></font size=2><br><br>
+<img src= https://4bes.nl/wp-content/uploads/2019/11/PSFunctionApp-300x252.png>
+<br> <br>
+<font size = 1>
+The information on this website is provided for informational purposes only and the authors make no warranties, either express or implied. <br>
+Information in these documents, including URL and other Internet Web site references, is subject to change without notice. <br>The entire risk of the use or the results from the use of this document remains with the user.<p>
+
+Microsoft, MS-DOS, Windows, Windows NT, and Windows Server are either registered trademarks or trademarks of Microsoft Corporation in the United States and/or other countries. <br>All other trademarks are property of their respective owners.
+</font size=1>
+</body>
+
 "@)
 
 $HTML = $HTML.ToString()
